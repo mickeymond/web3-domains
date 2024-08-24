e61f93787375993309ad3048ebaf8d623fbe7034
Michael Hammond <33749130+mickeymond@users.noreply.github.com>
2024-08-24 13:46:31 +0000

    Added web3 and chainlink plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css';
 import TokenSelector from "./components/TokenSelector";
+import { Web3 } from "web3";
+import { ChainlinkPlugin, MainnetPriceFeeds } from '@chainsafe/web3-plugin-chainlink';
+import { useEffect } from 'react';
+
+// const web3 = new Web3('https://rpc-sepolia.rockx.com');
+const web3 = new Web3();
+const chainlinkPlugin = new ChainlinkPlugin();
+
+web3.registerPlugin(chainlinkPlugin);
 
 function App() {
+  useEffect(() => {
+    // Get price
+    web3.chainlink.getPrice(MainnetPriceFeeds.BtcUsd)
+      .then(price => console.log(price.answer.toString()))
+      .catch(console.log);
+  }, []);
+
   return (
     <div className='min-h-full'>
       <header className="bg-white shadow">
